e1838e717688956982a44087a1fb3cb88cfd0530
ValentinaHutter <valentinahutter@gmail.com>
2026-08-19 10:53:30 +0200

    update s3 target path

--- eodc_eodag/collections/cop_dataspace_s3.py
+++ eodc_eodag/collections/cop_dataspace_s3.py
@@ -71,7 +71,7 @@ def get_cop_dataspace_s3_result(product_id=None):
                 print("RESULT: ", result)
                 break
         elif product_id.split("_")[-1] in ["B02.jp2", "B03.jp2", "B04.jp2", "B08.jp2", "TCI.jp2"]:
-            product_id_10 = product_id.replace(".jp2", "_60m.jp2")
+            product_id_10 = product_id.replace(".jp2", "_10m.jp2")
             if product_id_10.endswith(key):
                 result = content["Key"]
                 print("RESULT: ", result)
@@ -93,7 +93,22 @@ def stream_cop_dataspace_s3(s3_eodc, product, S3_BUCKET, product_id = None, prov
         provider = os.environ["PROVIDER"]
     if not collection:
         collection = os.environ["COLLECTION"]
-    s3_target = f"{provider}/{collection}/{item_id}/{product_id}"
+    if product_id.endswith(".jp2") and not product_id.startswith("MSK"):
+        re_str = re.search(
+            r"^(S2A|S2B|S2C|S2D)_(MSIL1C|MSIL2A)_(\d{8}T\d{6})_(N\d{4})_(R\d{3})_(.{6})_(\d{8}T\d{6})",
+            item_id
+        )
+        tile = re_str.group(6)
+        date = re_str.group(3)
+        if product_id.split("_")[-1] in ["B05.jp2", "B06.jp2", "B07.jp2", "B8A.jp2", "B11.jp2", "B12.jp2"]:
+            product_id = product_id.replace(".jp2", "_20m.jp2")
+        if product_id.split("_")[-1] in ["B01.jp2", "B09.jp2"]:
+            product_id = product_id.replace(".jp2", "_60m.jp2")
+        if product_id.split("_")[-1] in ["B02.jp2", "B03.jp2", "B04.jp2", "B08.jp2", "TCI.jp2"]:
+            product_id = product_id.replace(".jp2", "_10m.jp2")
+        s3_target = f"{provider}/{collection}/{item_id}/{tile}_{date}_{product_id}"
+    else:
+        s3_target = f"{provider}/{collection}/{item_id}/{product_id}"
     s3_eodc.upload_fileobj(product, Bucket=S3_BUCKET, Key=s3_target)
     print(f"Target path: {s3_target}")
     return
